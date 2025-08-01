2c9db1cbc611a6fbc08d480a25fbdbb0e40b9b73
김소영 <vkxqldtncprtm@naver.com>
2025-08-01 21:07:32 +0900

    fix layouts and pages

---
+++ js/00_common.js
@@ -0,0 +1,16 @@
+$(".menu, .snb").hover(
+  function () {
+    $(".snb").css("display", "block");
+  },
+  function () {
+    $(".snb").css("display", "none");
+  }
+);
+// fab
+ // 천천히 위로 사용법: $("html").animate({scrollTop: 0},시간);
+    $(function () {
+        $("#fab").click(function () {
+            // #fab 를 클릭하면
+            $("html").animate({scrollTop: 0},1000);
+        })
+    })
