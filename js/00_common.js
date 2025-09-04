95d44267a2018f27cfb6fd3ac3591bf698922096
김소영 <vkxqldtncprtm@naver.com>
2025-09-04 13:51:35 +0900

    change img&video files

--- js/00_common.js
+++ js/00_common.js
@@ -10,7 +10,7 @@ $(".menu, .snb").hover(
  // 천천히 위로 사용법: $("html").animate({scrollTop: 0},시간);
     $(function () {
         $("#fab").click(function () {
-            // #fab 를 클릭하면
-            $("html").animate({scrollTop: 0},1000);
+            //#fab 를 클릭하면
+            $("html,body").animate({scrollTop: 0},1000);
         })
     })
